369babf49c3aa490f827516bd2aa4cbc8f42b441
ToF- <cthibauttof@gmail.com>
2025-11-08 18:13:58 +0100

    basic turtle movement

--- src/turtle.lisp
+++ src/turtle.lisp
@@ -1,13 +1,55 @@
 ; a turtle is 4 state: pen (up or down), coords (x,y) and heading (in degrees)
 
+(defun make-turtle (pen coords heading)
+  (cons pen (cons coords (cons heading ()))))
+
 (defun new-turtle ()
-  (cons nil (list '((0.0 0.0) 90.0))))
+  (make-turtle nil (quote (0.0 0.0)) 0.0))
+
+(defun coords (turtle)
+  (cadr turtle))
 
 (defun pen-down? (turtle)
   (car turtle))
+
+(defun heading (turtle)
+    (caddr turtle))
 
 (defun pen-down (turtle)
   (cons t (cdr turtle)))
 
 (defun pen-up (turtle)
   (cons nil (cdr turtle)))
+
+(defun x (turtle)
+  (car (coords turtle)))
+
+(defun y (turtle)
+  (cadr (coords turtle)))
+
+(defun move (distance coords heading)
+  (let ((x (car coords))
+        (y (cadr coords)))
+    (cons
+      (+ x (* distance (cos (/ (* heading pi) 180.0))))
+      (cons
+        (+ y (* distance (sin (/ (* heading pi) 180.0))))
+        ()))))
+
+(defun forward (distance turtle)
+  (let ((pen (car turtle))
+        (coords (cadr turtle))
+        (heading (caddr turtle)))
+    (make-turtle pen (move distance coords heading) heading)))
+
+(defun right (angle turtle)
+  (let ((pen (car turtle))
+        (coords (cadr turtle))
+        (heading (caddr turtle)))
+    (make-turtle pen coords (- heading angle))))
+
+(defun left (angle turtle)
+  (let ((pen (car turtle))
+        (coords (cadr turtle))
+        (heading (caddr turtle)))
+    (make-turtle pen coords (+ heading angle))))
--- test/turtle-test.lisp
+++ test/turtle-test.lisp
@@ -14,5 +14,21 @@
              (assert-equal nil
                            (pen-down? (pen-up (pen-down (new-turtle))))))
 
+(define-test initially-turtle-at-zero
+             (assert-float-equal 0.0 (x (new-turtle)))
+             (assert-float-equal 0.0 (y (new-turtle))))
+
+(define-test after-forward-coord-change
+             (let ((f-turtle (forward 100 (new-turtle))))
+               (assert-float-equal 100.0 (x f-turtle))))
+
+(define-test after-turn-right-heading-change
+             (let ((r-turtle (right 45.0 (new-turtle))))
+               (assert-float-equal -45.0 (heading r-turtle))))
+
+(define-test after-turn-left-heading-change
+             (let ((r-turtle (left 45.0 (new-turtle))))
+               (assert-float-equal 45.0 (heading r-turtle))))
+
 (run-tests :all)
 (sb-ext:quit)
