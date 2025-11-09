bcff6a0f9859312c3952447ffb45211f0a64a2c4
Christophe Thibaut <cthibauttof@gmail.com>
2025-11-09 12:29:10 +0100

    trail up down forward

--- src/turtle.lisp
+++ src/turtle.lisp
@@ -1,7 +1,7 @@
-; a turtle is 4 state: pen (up or down), coords (x,y) and heading (in degrees)
+; a turtle is 4 state: pen (up or down), coords (x,y) and head (in degrees)
 
-(defun make-turtle (pen coords heading trail)
-  (cons pen (cons coords (cons heading (cons trail ())))))
+(defun make-turtle (pen coords head trail)
+  (cons pen (cons coords (cons head (cons trail ())))))
 
 (defun new-turtle ()
   (make-turtle nil (quote (0.0 0.0)) 0.0 ()))
@@ -15,29 +15,29 @@
 (defun pen-down? (turtle)
   (car turtle))
 
-(defun heading (turtle)
+(defun head (turtle)
     (caddr turtle))
 
 (defun trail (turtle)
   (cadddr turtle))
 
-(defun pen-down (turtle)
-  (let ((pen (pen turtle))
-        (coords (coords turtle))
-        (heading (heading turtle))
+(defun down (turtle)
+  (let ((coords (coords turtle))
+        (head (head turtle))
         (trail (trail turtle)))
-    (if pen
+    (if (pen turtle)
       turtle
-      (make-turtle
-        t
-        coords
-        heading
-        (cons
-          (cons coords ())
-          trail)))))
+      (make-turtle t coords head
+                   (cons '() trail)))))
 
-(defun pen-up (turtle)
-  (cons nil (cdr turtle)))
+(defun up (turtle)
+  (let ((coords (coords turtle))
+        (head (head turtle))
+        (trail (trail turtle)))
+  (if (pen turtle)
+    (make-turtle nil coords head
+        (cons (cons coords (car trail)) (cdr trail)))
+    turtle)))
 
 (defun x (turtle)
   (car (coords turtle)))
@@ -45,43 +45,43 @@
 (defun y (turtle)
   (cadr (coords turtle)))
 
-(defun move (distance coords heading)
+(defun move (distance coords head)
   (let ((x (car coords))
         (y (cadr coords)))
     (cons
-      (+ x (* distance (cos (/ (* heading pi) 180.0))))
+      (+ x (* distance (cos (/ (* head pi) 180.0))))
       (cons
-        (+ y (* distance (sin (/ (* heading pi) 180.0))))
+        (+ y (* distance (sin (/ (* head pi) 180.0))))
         ()))))
 
 (defun forward (distance turtle)
   (let ((pen (pen turtle))
         (coords (coords turtle))
-        (heading (heading turtle))
+        (head (head turtle))
         (trail (trail turtle)))
     (if pen
       (make-turtle
         pen
-        (move distance coords heading)
-        heading
+        (move distance coords head)
+        head
         (cons (cons coords (car trail)) (cdr trail)))
       (make-turtle
         pen
-        (move distance coords heading)
-        heading
+        (move distance coords head)
+        head
         trail))))
 
 (defun right (angle turtle)
   (let ((pen (pen turtle))
         (coords (coords turtle))
-        (heading (heading turtle))
+        (head (head turtle))
         (trail (trail turtle)))
-    (make-turtle pen coords (- heading angle) trail)))
+    (make-turtle pen coords (- head angle) trail)))
 
 (defun left (angle turtle)
   (let ((pen (car turtle))
         (coords (cadr turtle))
-        (heading (caddr turtle))
+        (head (caddr turtle))
         (trail (cadddr turtle)))
-    (make-turtle pen coords (+ heading angle) trail)))
+    (make-turtle pen coords (+ head angle) trail)))
 
--- test/turtle-test.lisp
+++ test/turtle-test.lisp
@@ -4,16 +4,6 @@
 (setq *print-failures* t)
 (load "src/turtle")
 
-(define-test new-turtle-has-pen-up
-             (assert-equal nil (pen-down? (new-turtle))))
-
-(define-test turtle-can-down-pen
-             (assert-equal t (pen-down? (pen-down (new-turtle)))))
-
-(define-test turtle-can-up-pen
-             (assert-equal nil
-                           (pen-down? (pen-up (pen-down (new-turtle))))))
-
 (define-test initially-turtle-at-zero
              (assert-float-equal 0.0 (x (new-turtle)))
              (assert-float-equal 0.0 (y (new-turtle))))
@@ -22,22 +12,49 @@
              (let ((f-turtle (forward 100 (new-turtle))))
                (assert-float-equal 100.0 (x f-turtle))))
 
-(define-test after-turn-right-heading-change
+(define-test after-turn-right-head-change
              (let ((r-turtle (right 45.0 (new-turtle))))
-               (assert-float-equal -45.0 (heading r-turtle))))
+               (assert-float-equal -45.0 (head r-turtle))))
 
-(define-test after-turn-left-heading-change
+(define-test after-turn-left-head-change
              (let ((r-turtle (left 45.0 (new-turtle))))
-               (assert-float-equal 45.0 (heading r-turtle))))
+               (assert-float-equal 45.0 (head r-turtle))))
 
 (define-test initially-trail-is-empty
              (assert-equal t (null (trail (new-turtle)))))
 
+(define-test after-one-move-with-pen-up-trail-unchanged
+             (let ((f-turtle (forward 100 (up (new-turtle)))))
+               (assert-equal '() (trail f-turtle))))
+
 (define-test after-one-move-with-pen-down-trail-is-not-empty
-             (let ((t-turtle (move 100.0 (pen-down (new-turtle)))))
-               (assert-equal 
-                 '((100.0 0.0) (0.0 0.0))
-                 (car (trail t-turtle)))))
+             (let ((f-turtle (forward 100 (down (new-turtle)))))
+               (assert-equal '(((0.0 0.0))) (trail f-turtle))))
+
+(define-test pen-up-finishes-current-line-in-trail
+             (let ((f-turtle
+                     (up
+                       (forward 100
+                                (down (new-turtle))))))
+               (assert-equal
+                 '(((100.0d0 0.0d0) (0.0 0.0)))
+                 (trail f-turtle))))
+
+(define-test pen-down-starts-a-new-line-in-trail
+             (let ((f-turtle
+                     (up
+                       (forward 100
+                                (down
+                                  (forward 50
+                                           (up
+                                             (forward 200
+                                                      (down (new-turtle))))))))))
+               (assert-equal
+                 '(
+                    ((350.0d0 0.0d0) (250.0d0 0.0d0))
+                    ((200.0d0 0.0d0) (0.0 0.0))
+                    )
+                 (trail f-turtle))))
 
 (run-tests :all)
 (sb-ext:quit)
